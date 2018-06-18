576918d9dfcb06c8e55651b4c1bbd754c5a42b6b
Dylan McDonald <dmcdonald21@cnm.edu>
2018-06-18 16:56:42 -0600

    added get to reducer

--- src/app/reducers/photo.reducer.ts
+++ src/app/reducers/photo.reducer.ts
@@ -2,6 +2,7 @@ import {ActionWithPayload} from './todo.reducer';
 
 export const ADD_PHOTO = 'ADD_PHOTO';
 export const DELETE_PHOTO = 'DELETE_PHOTO';
+export const GET_PHOTOS = 'GET_PHOTOS';
 
 export interface Photo {
    albumId?: number;
@@ -17,6 +18,8 @@ export function photoReducer(state = [], action: ActionWithPayload<Photo>) {
          return[action.payload, ...state];
       case DELETE_PHOTO:
          return state.filter(item => item.id !== action.payload.id);
+      case GET_PHOTOS:
+         return[action.payload];
       default:
          return state;
    }
