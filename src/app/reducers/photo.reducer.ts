700535a7c1198acb4f66d7b6c76b65755bbe1a10
Dylan McDonald <dmcdonald21@cnm.edu>
2018-06-18 17:52:26 -0600

    added success to photo reducer strings

--- src/app/reducers/photo.reducer.ts
+++ src/app/reducers/photo.reducer.ts
@@ -3,6 +3,7 @@ import {ActionWithPayload} from './todo.reducer';
 export const ADD_PHOTO = 'ADD_PHOTO';
 export const DELETE_PHOTO = 'DELETE_PHOTO';
 export const GET_PHOTOS = 'GET_PHOTOS';
+export const GET_PHOTOS_SUCCESS = 'GET_PHOTOS_SUCCESS';
 
 export interface Photo {
    albumId?: number;
@@ -18,7 +19,7 @@ export function photoReducer(state = [], action: ActionWithPayload<Photo>) {
          return[action.payload, ...state];
       case DELETE_PHOTO:
          return state.filter(item => item.id !== action.payload.id);
-      case GET_PHOTOS:
+      case GET_PHOTOS_SUCCESS:
          return[action.payload];
       default:
          return state;
