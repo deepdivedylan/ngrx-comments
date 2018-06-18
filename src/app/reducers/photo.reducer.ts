5640172187802667a64d3b6e0837c93e07a4e216
Dylan McDonald <dmcdonald21@cnm.edu>
2018-06-18 16:28:13 -0600

    added photo reducer

---
+++ src/app/reducers/photo.reducer.ts
@@ -0,0 +1,23 @@
+import {ActionWithPayload} from './todo.reducer';
+
+export const ADD_PHOTO = 'ADD_PHOTO';
+export const DELETE_PHOTO = 'DELETE_PHOTO';
+
+export interface Photo {
+   albumId?: number;
+   id?: number;
+   title?: string;
+   url?: string;
+   thumbnailUrl?: string;
+}
+
+export function PhotoReducer(state = [], action: ActionWithPayload<Photo>) {
+   switch (action.type) {
+      case ADD_PHOTO:
+         return[action.payload, ...state];
+      case DELETE_PHOTO:
+         return state.filter(item => item.id !== action.payload.id);
+      default:
+         return state;
+   }
+}
